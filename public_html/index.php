2021d93ac0fb9a585310703dc6ce05b85ce5601e
tiger154 <junhwen@naver.com>
2015-02-14 22:56:27 +0900

    from aptana

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,7 @@
 
 /**
  * 시스템 파일 
+ * System Config File
  */
 $_DOC_ROOT = substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], "/"));
 require_once $_DOC_ROOT."/engine/_sys/sys.conf.php";
@@ -25,11 +26,13 @@ error_reporting(_ERROR_REPORTING);
 
 /**
  * 처리시간측정-시작
+ * Start estamate process time
  */
 $__sTime = Module::getMicroTime();
 
 /**
  * 기본클래스
+ * Base Class init
  */
 $TPL = &Module::singleton("Template");
 $BASE = &Module::singleton("Base");
@@ -37,12 +40,14 @@ $BASE = &Module::singleton("Base");
 
 /**
  * 사이트설정정보
+ * Set Site Info
  */
 $SITE = Module::loadConf(_INI_SITE);
 $TPL->setValue(array("SITE"=>$SITE));
 
 /**
  * 템플릿경로
+ * Set Template location
  */
 $TPL->setValue(array("DOMAIN"=>"http://"._DOMAIN));
 $TPL->setValue(array("DOMAIN_FILE"=>"http://"._DOMAIN_FILE));
@@ -53,6 +58,7 @@ $TPL->setValue(array("CSS"=>"http://"._DOMAIN."/css"));
  
 /**
  * 세션설정
+ * Set Session 
  */
 ini_set("session.cookie_domain", _DOMAIN);
 session_cache_limiter('nocache, must-revalidate');
@@ -60,21 +66,50 @@ session_set_cookie_params(0, "/", _DOMAIN);
 session_start();
 $TPL->setValue($_SESSION);	 // 세션변수설정
 /**
- * 모듈로드 
+ * 모듈로드
+ * 
+ *  1. JMVC Framework 
+ *  - summary : To make easy and faste some website we can use EasyMVC Framework 
+ *              it has MVC pattern and include Template engine(http://~~)
+ *				 
+ * 
+ *  First of all an action is a page and a controller , so unfortunally we can't use 상속... now, 
+ *  in controller we can do like general MVC framework do . it means u can code and define a view 
+ *  also as you know model is same like u know.
+ *  
+ *  2. RouteRule  
+ *   - firstly we use strict Route rules which we encourage you to follow . However there are opportunities for adaption.
+ *   - every request to server route to index.php first by .htaccess
+ *  
+ *   1) example 
+ *     - below see you can understand more easy also you can change all this location in /engine/_sys/sys.config
+ *     URI			: /SomeModule/SomeAction
+ *     Controller	: /Controller/SomeModule/SomeModule.SomeAction.php
+ *     View			: /view/SomeModule/SomeModule.SomeAction.htm 
+ * 
+ *   2) Parameter 
+ *    -> after second item will be parameters so you can add parameter all you want  
+ * 
+ *  3. TemplateEngine 
+ *    - By TemlpateEngine we can use like VIEW 
+ *  
  */
-Module::routeModule(); // 권한체크포함
+Module::routeModule(); // Route and load module , it parse every request as module for example if u request /SomeModule/SomeAction it load SomeModule as a Module
 $TPL->setValue(array("MODULE"=>Module::$module));
 $TPL->setValue(array("TODO"=>Module::$todo));
 $TPL->setValue(array("REQUEST_URI"=>$_SERVER["REQUEST_URI"]));
 
 /**
  * 시작페이지(모듈명변경) / 파킹페이지 설정
+ *  Set ParkigPage
+ *  Set StartPage
  */
 Module::parking(Module::$module);
 Module::startPage(Module::$module);
 
 /**
  * 디자인프레임설정
+ *  - routeModule function set a frame that is global var and used as Template Frame 
  */
 switch($FRAME) {
 	case "none" :
@@ -171,11 +206,13 @@ switch($FRAME) {
 
 /**
  * 템플릿최종변수셋팅
+ * Assign All value
  */
 $TPL->assignValue();
 
 /**
  * 디자인프레임에 따른 페이지 출력
+ * Print Template 
  */
 switch($FRAME) {
 	case "none" : // 템플릿출력안함
@@ -192,6 +229,7 @@ switch($FRAME) {
 
 /**
  * 처리시간측정-종료
+ * estimate process time
  */
 $__eTime = Module::getMicroTime();
 Module::runTimeMsg($__sTime, $__eTime);
